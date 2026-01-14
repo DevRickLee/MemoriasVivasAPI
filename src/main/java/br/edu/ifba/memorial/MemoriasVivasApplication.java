f7a464eacfed77a613176c4fdbc5e6c562757e3b
Wérick Santos <202411240021@ifba.edu.br>
2026-01-14 17:19:51 -0300

    Commit inicial

---
+++ src/main/java/br/edu/ifba/memorial/MemoriasVivasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.memorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriasVivasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoriasVivasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifba/memorial/MemoriasVivasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.memorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoriasVivasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
